fe78cf1d835a5caa7ef03d476dd861e40085607d
prahal <vprahaladhan@gmail.com>
2021-06-16 22:16:50 +1000

    commit tree 000856c68d18ff7559d1d02caa0b90448f26a481

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,9 @@ ActiveRecord::Schema.define(version: 20210615040923) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.string   "password_digest"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
