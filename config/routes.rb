fbe0df51df5cf97ae13f60be8de3af89aa512064
prahal <vprahaladhan@gmail.com>
2021-06-16 22:25:32 +1000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   # resources :sessions
   get '/login'  => 'sessions#new'
-  post '/login' => 'sessions#create'
+  post '/login', to: 'sessions#create', as: 'login_post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
